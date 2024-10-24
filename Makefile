52ed6572a5ee2693d3c9b50f89c7f20e9099caf8
abdullah khalid <138355319+Abdullahkhalid05@users.noreply.github.com>
2024-10-25 03:09:44 +0500

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,3 @@
+run:
+	gcc pacman.c -o pacman -lraylib
+	./pacman
---
+++ pacman.c
@@ -0,0 +1,83 @@
+
+
+#include <raylib.h>
+
+#define ROWS 19
+#define COLS 16
+#define CELL_SIZE 40
+
+void drawBoard(char pacmanBoard[ROWS][COLS]){
+   for (int i = 0; i < ROWS; i++) {
+      for (int j = 0; j < COLS; j++) {
+       Vector2 position = {j * CELL_SIZE , i * CELL_SIZE};
+       if (pacmanBoard[i][j] == '#') {
+         DrawRectangle(position.x, position.y, CELL_SIZE, CELL_SIZE, BLUE);
+       }
+       else if (pacmanBoard[i][j] == '.') {
+         DrawCircle(position.x + CELL_SIZE / 2, position.y + CELL_SIZE / 2, 5, YELLOW);
+       }
+       else if (pacmanBoard[i][j] == 'Q') {
+         DrawText("Q", position.x + CELL_SIZE / 4, position.y + CELL_SIZE / 4, CELL_SIZE / 2, RED);
+       }
+     }
+   }
+}
+
+int main(){
+    const int screenWidth = COLS * CELL_SIZE;
+    const int screenHeight = ROWS * CELL_SIZE;
+
+     InitWindow(screenWidth , screenHeight , "Pacman");
+     SetTargetFPS(60);
+      int qx = 7;
+      int qy = 7;
+      int score = 0;
+     char pacmanBoard[ROWS][COLS] = {
+       {'#' , '#' , '#' , '#' , '#' ,'#' ,'#' ,'#' , '#' , '#' ,'#' , '#' , '#' , '#' , '#' , '#'},
+       {'#' , '.' , '.' , '.' , '.' , '.' , '.' , '.' , '.' , '.' , '.' , '.', '.', '.', '.', '#' },
+       {'#' , '.' , '.' , '.' , '.' , '.' , '.' , '.' , '.' , '.' , '.' , '.', '.', '.', '.', '#' },
+       {'#' , '.' , '.' , '.' , '.' , '.' , '.' , '.' , '.' , '.' , '.' , '.', '.', '.', '.', '#' },
+       {'#' , '.' , '.' , '#' , '.' , '.' , '.' , '.' , '.' , '.' , '.' , '.', '#', '.', '.', '#' },
+       {'#' , '.' , '.' , '#' , '.' , '.' , '.' , '.' , '.' , '.' , '.' , '.', '#', '.', '.', '#' },
+       {'#' , '.' , '.' , '#' , '.' , '.' , '.' , '.' , '.' , '.' , '.' , '.', '#', '.', '.', '#' },
+       {'#' , '.' , '.' , '#' , '.' , '.' , '.' , 'Q' , '.' , '.' , '.' , '.', '#', '.', '.', '#' },
+       {'#' , '.' , '.' , '#' , '.' , '.' , '.' , '.' , '.' , '.' , '.' , '.', '#', '.', '.', '#' },
+       {'#' , '.' , '.' , '#' , '.' , '.' , '.' , '.' , '.' , '.' , '.' , '.', '#', '.', '.', '#' },
+       {'#' , '.' , '.' , '#' , '#' , '#' , '#' , '#' , '#' , '#' , '#' , '#', '#', '.', '.', '#' },
+       {'#' , '.' , '.' , '.' , '.' , '.' , '.' , '.' , '.' , '.' , '.' , '.', '.', '.', '.', '#' },
+       {'#' , '.' , '.' , '.' , '.' , '#' , '#' , ' ' , ' ' , '#' , '#' , '.', '.', '.', '.', '#' },
+       {'#' , '.' , '.' , '.' , '.' , '#' , ' ' , ' ' , ' ' , ' ' , '#' , '.', '.', '.', '.', '#' },
+       {'#' , '.' , '.' , '.' , '.' , '#' , ' ' , ' ' , ' ' , ' ' , '#' , '.', '.', '.', '.', '#' },
+       {'#' , '.' , '.' , '.' , '.' , '#' , '#' , '#' , '#' , '#' , '#' , '.', '.', '.', '.', '#' },
+       {'#' , '.' , '.' , '.' , '.' , '.' , '.' , '.' , '.' , '.' , '.' , '.', '.', '.', '.', '#' },
+       {'#' , '.' , '.' , '.' , '.' , '.' , '.' , '.' , '.' , '.' , '.' , '.', '.', '.', '.', '#' },
+       {'#' , '#' , '#' , '#' , '#' ,'#' ,'#' ,'#' , '#' , '#' ,'#' , '#' , '#' , '#' , '#' , '#' },
+    };
+    while (!WindowShouldClose()) {
+  
+     if (IsKeyPressed(KEY_UP) && pacmanBoard[qy - 1][qx] != '#') {
+       //if (); 
+       pacmanBoard[qy][qx] = ' ';
+       qy--;
+     }
+     if (IsKeyPressed(KEY_DOWN) && pacmanBoard[qy + 1][qx] != '#') {
+       pacmanBoard[qy][qx] = ' ';
+       qy++;
+     }
+     if (IsKeyPressed(KEY_LEFT) && pacmanBoard[qy][qx - 1] != '#') {
+       pacmanBoard[qy][qx] = ' ';
+       qx--;
+     }
+     if (IsKeyPressed(KEY_RIGHT) && pacmanBoard[qy][qx + 1] != '#') {
+       pacmanBoard[qy][qx] = ' ';
+       qx++;
+     }
+       pacmanBoard[qy][qx] = 'Q';
+      BeginDrawing();
+      ClearBackground(BLACK);
+      drawBoard(pacmanBoard);
+      EndDrawing();
+    }
+    CloseWindow();
+    return 0;
+}
